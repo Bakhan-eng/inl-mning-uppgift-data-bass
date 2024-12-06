922b116e52a1f06bb39a4d3e466ae644382684d4
Bakhan-eng <baxanb15@yahoo.com>
2024-12-06 10:34:29 +0100

    Context

--- BakhanInlämning/Data/DbContext.cs
+++ BakhanInlämning/Data/DbContext.cs
@@ -8,9 +8,27 @@ public class DbContextUppgift : DbContext
         public DbSet<Book> Books { get; set; }
         public DbSet<BookAuthor> BookAuthors { get; set; }
         public DbSet<Loan> Loans { get; set; }
-        
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
                 optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Library;Trusted_Connection=True;");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasMany(p=> p.BookAuthors)
+                .WithOne(a => a.Book)
+                .HasForeignKey(p => p.BookId);
+
+            modelBuilder.Entity<Author>()
+                .HasMany(p=> p.BookAuthors)
+                .WithOne(a => a.Author)
+                .HasForeignKey(p => p.AuthorId);
+
+            modelBuilder.Entity<Loan>()
+                .HasMany(p=> p.Books)
+                .WithOne(a => a.Loan)
+                .HasForeignKey(p => p.Loanid);
+    }
 }
